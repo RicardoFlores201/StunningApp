f9a10c0bd32400ae534e6391d9a89a5d79b1cd82
richardram233 <richardram233@gmail.com>
2026-05-09 17:52:30 -0600

    Trend item

--- app/src/main/java/com/ejercicio/stunningapp/ui/components/CommonComponents.kt
+++ app/src/main/java/com/ejercicio/stunningapp/ui/components/CommonComponents.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -24,12 +26,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.ejercicio.stunningapp.ui.model.Trend
 import com.ejercicio.stunningapp.ui.theme.CategoryOvalColor
+import com.ejercicio.stunningapp.ui.theme.DarkBlue
+import com.ejercicio.stunningapp.ui.theme.TextGray
 
 @Composable
 fun AnimatedItem(
@@ -92,6 +98,48 @@ fun CategoryItem(
                 fontSize = 14.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
+            )
+        }
+    }
+}
+
+@Composable
+fun TrendItem(
+    trend: Trend
+){
+    Card(
+        modifier = Modifier
+            .width(205.dp)
+            .height(260.dp)
+            .padding(bottom = 8.dp),
+        shape = RoundedCornerShape(10.dp),
+        elevation = CardDefaults.cardElevation(3.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
+    ) {
+        Column(
+
+        ) {
+            Image(
+                painter = painterResource(id = trend.picRes),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth().height(160.dp).clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = trend.title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                color = TextGray,
+                maxLines = 2
+            )
+            Text(
+                text = trend.subtitle,
+                fontSize = 12.sp,
+                color = DarkBlue,
+                modifier = Modifier.padding(8.dp, vertical = 8.dp)
             )
         }
     }
