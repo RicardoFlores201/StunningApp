b0ed64d8b083c611c9c0173371a3882ebab46a03
richardram233 <richardram233@gmail.com>
2026-05-09 17:51:59 -0600

    Trend items and section

---
+++ app/src/main/java/com/ejercicio/stunningapp/ui/model/Trend.kt
@@ -0,0 +1,7 @@
+package com.ejercicio.stunningapp.ui.model
+
+data class Trend(
+    val title: String,
+    val subtitle: String,
+    val picRes: Int
+)
---
+++ app/src/main/java/com/ejercicio/stunningapp/ui/sections/TrendSection.kt
@@ -0,0 +1,58 @@
+package com.ejercicio.stunningapp.ui.sections
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ejercicio.stunningapp.R
+import com.ejercicio.stunningapp.ui.components.TrendItem
+import com.ejercicio.stunningapp.ui.model.Trend
+import com.ejercicio.stunningapp.ui.theme.DarkBlue
+
+@Composable
+fun TrendSection(){
+    val trends = listOf(
+        Trend("Futuro de la IA", "", R.drawable.trends),
+        Trend("", "", R.drawable.trends2),
+        Trend("", "", R.drawable.trends)
+    )
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp)
+                .padding(top = 32.dp)
+        ) {
+            Text(
+                text = "Tendencias",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = DarkBlue,
+                modifier = Modifier.clickable{}
+            )
+
+            LazyRow(
+                contentPadding = PaddingValues(horizontal = 24.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(trends){
+                    item ->
+                    TrendItem(trend = item)
+                }
+            }
+        }
+    }
+}
